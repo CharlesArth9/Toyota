b026facb0e879dcb063dd84220e12c780a09dc35
HDI <HDI@CHMEP5CD9326Y5J.corp.choucairtesting.local>
2021-06-15 13:18:05 -0500

    Con dataTable

--- src/test/java/co/com/toyota/definitions/ToyotaDefinitions.java
+++ src/test/java/co/com/toyota/definitions/ToyotaDefinitions.java
@@ -1,6 +1,10 @@
 package co.com.toyota.definitions;
 
 import co.com.toyota.steps.ToyotaSteps;
+import cucumber.api.DataTable;
+import java.util.List;
+import java.util.Map;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -15,15 +19,21 @@ public class ToyotaDefinitions {
         toyota.openWebsite();
     }
 
-    @When("^try to choose a car model$")
-    public void tryToChooseACarModel()  {
-        toyota.selectCarBanner("5");
-        toyota.chooseModelSpecification("HILUX 2.4L");
+
+    @When("^try to choose a option for a car model in the page$")
+    public void tryToChooseAOptionForACarModelInThePage(DataTable table) {
+        List<Map<String, String>> data = table.asMaps(String.class, String.class);
+        String option = data.get(0).get("Options");
+        String model = data.get(0).get("Models");
+
+        toyota.selectCarBanner(option);
+        toyota.chooseModelSpecification(model);
     }
 
     @Then("^check engine information$")
-    public void checkEngineInformation()  {
+        public void checkEngineInformation()  {
         toyota.printInfoMotor();
     }
+
 
 }
--- src/test/resources/features/search_motor_toyota.feature
+++ src/test/resources/features/search_motor_toyota.feature
@@ -1,10 +1,16 @@
 @navigation
 Feature: Perform the navigation of a flow that allows me to visualize the engine specifications of
-         a car model on the toyota.com.co website
+  a car model on the toyota.com.co website
 
 
   @success
-  Scenario: view details of the engine of a car model
+  Scenario Outline: view details of the engine of a car model
     Given Carlos enters the toyota.com.co website
-    When try to choose a car model
+    When try to choose a option for a car model in the page
+      | Options   | Models   |
+      | <Options> | <Models> |
     Then check engine information
+    Examples:
+      | Options | Models      |
+      | 5       | HILUX 2.8L  |
+      | 8       | YARIS SPORT |
